5312e13ec6bddff12ce81ea9c7489a9b63a5c5b1
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-04-15 23:20:44 +0300

    11:20

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+
 'use strict';
+
 const express = require('express');
 var cors = require('cors');
 const axios = require('axios');
@@ -6,8 +8,11 @@ const { request } = require('express');
 require('dotenv').config();
 const { Client } = require('pg');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const qureyString = require("querystring");
+
 //environment variables
+// IMPORTANT! create your .env file
 const PORT = process.env.PORT;
 const apikey = process.env.API_KEY;
 const dataBaseUrl = process.env.DATABASE_URL;
@@ -22,17 +27,26 @@ app.use(bodyParser.json());
 
 //---SQL Routes---
 
-//GET Routes
-app.get("/recipes", recipesHandler); //(you can delete the test rout after you make sure every thing is ok)
-app.get("/getAllRecipes", getAllRecipesHandler)
-app.get ('/allIngredient', allIngredientHandler)
+//GET Routes sql
+app.get("/allRecipes", getAllRecipesHandler)
+app.get ('/allIngredients', allIngredientHandler)
 
-//POST Routs
+//POST Routs sql
 app.post("/addNewRecipe", addNewRecipesHandler)
 app.post('/addIngredient', addNewIngredientHandler);
 
-//PUT Routs
+//PUT Routs sql
 app.put('/updateIngredient/:id',updateHandler)
+
+//GET Routs api
+
+app.get('/recipeAnalyzedInstructions', analyzedInstructionsHandler);
+
+app.get('/AutoComplete', autoCompleteHandler);
+
+
+
+app.use("*", notFoundErrorHandler)// make sure to always make it the last route 
 
 
 //---API Routes---
@@ -187,11 +201,90 @@ function formatDataToPattern(data) {
   }
     
 
+//general functions
+
+function notFoundErrorHandler(req, res) {
+    res.status(404).send("Route not found")
+}
+
+function serverErrorHadnler(req, res, error = "Sorry, something went wrong") {
+    res.status(500).send({
+        "status": 500,
+        "response": error
+    });
+    console.log(error);
+}
+
+//API Handlers
+
+function analyzedInstructionsHandler(req, res) {
+
+    let url = `https://api.spoonacular.com/recipes/${qureyString.stringify(req.body)}/analyzedInstructions?apiKey=${apikey}`;
+    axios.get(url)
+        .then((result) => {
+            console.log(result.data);
+            let response = result.data;
+            res.json(response);
+        })
+        .catch((error) => {
+            serverErrorHadnler(req, res, error);
+        })
+}
+
+function autoCompleteHandler(req, res) {
+
+    let url = `https://api.spoonacular.com/food/ingredients/autocomplete?${qureyString.stringify(req.body)}&apiKey=${apikey}`
+    axios.get(url)
+        .then((result) => {
+            console.log(result.data);
+            let response = result.data;
+            res.json(response);
+        })
+        .catch((error) => {
+            serverErrorHadnler(req, res, error);
+        })
+
+}
+
+//SQL Handlers
+
+function getAllRecipesHandler(req, res) {
+
+    let { userID } = req.body;
+    let values = [userID];
+    let query = `select * from ${recipes_table} where userID=$1`;
+    client.query(query, values, (error, sqlResult) => {
+        if (error) {
+            serverErrorHadnler(req, res, error);
+        }
+        else
+            res.json(sqlResult.rows);
+    })
+}
+
+function addNewRecipesHandler(req, res) {
+
+    let { id, title, item_image, userID } = req.body;
+    let values = [id, title, item_image, userID];
+    let query = `INSERT INTO ${recipes_table} (id, title, item_image,userID)
+        VALUES ($1,$2,$3,$4) returning *`;
+    client.query(query, values, (error, sqlResult) => {
+        if (error) {
+            serverErrorHadnler(req, res, error);
+        }
+        else
+            res.json(sqlResult.rows);
+    })
+}
+
+//server start section
 
 client.connect().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is listening ${PORT}`);
     })
-        }).catch((err) => {
-            console.log(err)
-        })
+
+}).catch((error) => {
+
+    console.log(error);
+})
