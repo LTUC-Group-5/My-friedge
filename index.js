7167193ea9b673e76c4bdc2d43c0ef5ac2acc6a2
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-04-15 18:06:48 +0300

    getAllRecipes+addNewRecipe

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 'use strict';
-const express = require('express')
+const express = require('express');
 var cors = require('cors');
 const axios = require('axios');
 const { request } = require('express');
 require('dotenv').config();
-const { Client } = require('pg')
+const { Client } = require('pg');
 const app = express();
-app.use(cors());
 const bodyParser = require('body-parser')
+//environment variables
 const PORT = process.env.PORT;
-const apikey=process.env.API_KEY
-const dataBaseUrl=process.env.DATABASE_URL 
+const apikey = process.env.API_KEY;
+const dataBaseUrl = process.env.DATABASE_URL;
+const ingredients_table = process.env.INGREDIENT_TABLE;
+const recipes_table = process.env.RECIPE_TABLE;
+
 const client = new Client(process.env.DATABASE_URL)
+//app useages
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-//postgres://username:sudo password@localhost:5432/database name
-//postgres://ibraheem:0000@localhost:5432/movies
 
 //create your .env file
 
 
 
 
-//http://localhost:portnumber/TestRout (you can delete the test rout after you make sure every thing is ok)
-app.get('/TestRout', TestRoutHandler);
 
-//Routs
+//GET Routs
 app.get("/recipes", recipesHandler); //(you can delete the test rout after you make sure every thing is ok)
 
+app.get("/getAllRecipes", getAllRecipesHandler)
 
+//POST Routs
+
+app.post("/addNewRecipe", addNewRecipesHandler)
 
 
 
@@ -48,54 +53,80 @@ function TestRoutHandler(req, res) {   //(you can delete the TestRoutHandler fun
     res.send("The server is alive")
 }
 
-function handleNtFoundError(req, res){ 
-    res.status(404).send("Rout not found") 
+function handleNtFoundError(req, res) {
+    res.status(404).send("Route not found")
 }
 
+function serverErrorHadnler(req, res, err = "Sorry, something went wrong") {
+    res.status(500).send({
+        "status": 500,
+        "response": err
+    });
+    console.log(err);
+}
 
-function recipesHandler(req, res){
-    
+function recipesHandler(req, res) {
+
     let url = `https://api.spoonacular.com/recipes/random?apiKey=${apikey}`;
     axios.get(url)
-    .then((result)=>{
-        console.log(result.data.recipes);
+        .then((result) => {
+            console.log(result.data.recipes);
 
-        let dataRecipes = result.data.recipes.map((recipe)=>{
-            return new Recipe(recipe.title, recipe.readyInMinutes,recipe.image)
+            let dataRecipes = result.data.recipes.map((recipe) => {
+                return new Recipe(recipe.title, recipe.readyInMinutes, recipe.image)
+            })
+
+            res.json(dataRecipes);
         })
-        
-        res.json(dataRecipes);
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
+        .catch((err) => {
+            console.log(err);
+        })
 
 }
 
 
+function getAllRecipesHandler(req, res) {
+    let { userID } = req.body;
+    let query = `select * from ${recipes_table} where userID=${userID}`;
+    client.query(query)
+        .then((queryRes) => {
+            res.json(queryRes.rows)
+        })
+        .catch((err) => {
+            serverErrorHadnler(req, res, err);
+        })
+}
 
-
+function addNewRecipesHandler(req, res) {
+    let { id, title, item_image, userID } = req.body;
+    let values = [id, title, item_image, userID];
+    let query = `INSERT INTO ${recipes_table} (id, title, item_image,userID)
+        VALUES ($1,$2,$3,$4) returning *`;
+    client.query(query, values, (error, sqlResult) => {
+        if (error) {
+            serverErrorHadnler(req,res,error);
+        }
+        else
+        res.json(sqlResult.rows);
+      })
+    }
 
 
 //constructor
-function Recipe(title,time,image){
-    this.title=title;
-    this.time=time;
-    this.image=image;
-}
-
-
-
-
-
+function Recipe(title, time, image) {
+            this.title = title;
+            this.time = time;
+            this.image = image;
+        }
 
 
 client.connect().then(() => {
 
-    app.listen(PORT, () => {
-        console.log(`Server is listening ${PORT}`);
-    })
-    
-}).catch((err)=>{
+            app.listen(PORT, () => {
+                console.log(`Server is listening ${PORT}`);
+            })
 
-console.log(err)})
+        }).catch((err) => {
+
+            console.log(err)
+        })
