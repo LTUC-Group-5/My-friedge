91ded61dd0cc3e8c5f49b7d3546c77524c66e060
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-04-16 01:51:57 +0300

    Revert "final submition"
    This reverts commit 4b406a14a52c7534ea6487f4697598a6a96259b8.

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
 create table favorite_ingredient(
 id int PRIMARY KEY,
-item_name varchar(500),
-item_image varchar(500),
+_name varchar(500),
+_image varchar(500),
 quantity int DEFAULT 1,
-userID int DEFAULT 1
+userID int null
 );
 
 create table favorite_recipe(
 id int PRIMARY KEY,
 title varchar(500),
-item_image varchar(500),
-userID int DEFAULT 1
+_image varchar(500),
+userID int null
 );
